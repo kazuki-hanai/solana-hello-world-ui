03cad8211e749de5df11b0027aad162cd4a5bd67
wan-nyan-wan <wan-nyan-wan@users.noreply.github.com>
2021-02-10 13:06:28 +0000

    deploy: 5acbac1c49c747100f2179836378afaa10f16abe

--- _dist_/components/content/index.js
+++ _dist_/components/content/index.js
@@ -2,8 +2,9 @@ import React from "../../../_snowpack/pkg/react.js";
 import {Header} from "../header/index.js";
 import {HelloWorld} from "../helloWorld/index.js";
 import {Description} from "../description/index.js";
+import {Footer} from "../footer/index.js";
 export const Content = () => {
   return /* @__PURE__ */ React.createElement("div", {
     className: "flex flex-col place-content-center justify-center min-h-screen"
-  }, /* @__PURE__ */ React.createElement(Header, null), /* @__PURE__ */ React.createElement(Description, null), /* @__PURE__ */ React.createElement(HelloWorld, null));
+  }, /* @__PURE__ */ React.createElement(Header, null), /* @__PURE__ */ React.createElement(Description, null), /* @__PURE__ */ React.createElement(HelloWorld, null), /* @__PURE__ */ React.createElement(Footer, null));
 };
---
+++ _dist_/components/footer/index.js
@@ -0,0 +1,9 @@
+import React from "../../../_snowpack/pkg/react.js";
+export const Footer = () => {
+  return /* @__PURE__ */ React.createElement("footer", {
+    className: "text-white text-center"
+  }, /* @__PURE__ */ React.createElement("p", null, "Repository URL:", " ", /* @__PURE__ */ React.createElement("a", {
+    className: "underline",
+    href: "https://github.com/wan-nyan-wan/solana-hello-world-ui"
+  }, "https://github.com/wan-nyan-wan/solana-hello-world-ui")));
+};
